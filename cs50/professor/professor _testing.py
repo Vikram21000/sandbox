6b412a9f9de96c0c63702daf2af4a096b72fadc0
Vikram J <148868830+Vikram21000@users.noreply.github.com>
2025-05-28 17:12:46 +0100

    Created a semi-working version of professor.py

--- cs50/professor/professor _testing.py
+++ cs50/professor/professor _testing.py
@@ -1,10 +1,50 @@
 import random
 
-def get_level():
+try:
+    counter = 10
     level = int(input("Level: "))
-    if level < 3:
-        return level
-    else:
-        pass
 
+    if level < 1 or level > 3:
+        print("Somthing's up with level validation")
+        raise ValueError
+
+    if level == 1:
+        for i in range(1,counter+1):
+            x = random.randint(1,9)
+            y = random.randint(1,9) 
+            print(f"{x} + {y} = ")
+            response = int(input())
+            if response != (x+y):
+                for i in range(1,4):
+                    print(f"{x} + {y} = ")
+                    response = int(input())
+                    if response == (x+y):
+                        continue
+                    else:
+                        continue
+            counter += 1
+    elif level == 2:
+        for i in range(1,counter+1):
+            x = random.randint(10,99)
+            y = random.randint(10,99) 
+            print(f"{x} + {y} = ")
+            response = int(input())
+            counter += 1
+    elif level == 3:
+        for i in range(1,counter+1):
+            x = random.randint(100,999)
+            y = random.randint(100,999) 
+            print(f"{x} + {y} = ")
+            response = int(input())
+            counter += 1              
+        
+
+    else:
+        print("Something's up with level generation")
+        raise ValueError
+
+
+
+except ValueError:
+    print("Something went wrong :(")
 
--- cs50/professor/professor.py
+++ cs50/professor/professor.py
@@ -2,34 +2,44 @@ import random
 
 
 def main():
-    ...
+    get_level()
+    generate_integer()
 
 
 def get_level():
-    try:
-        level = int(input("Level: "))
-        if 0 < level < 4:
-            return level
-    except ValueError:
-        pass
+    level = int(input("Level: "))
+    if level < 1 or level > 3:
+        print("Somthing's up with level validation")
+        raise ValueError
+    else:
+        return level
 
 def generate_integer(level):
-
     if level == 1:
-        x = random.randint(1,9)
-        y = random.randint(1,9)
-
-        print(f"{x} + {y} = {answer}", end = "")
-        answer = int(input())
-
+        for i in range(1,counter+1):
+            x = random.randint(1,9)
+            y = random.randint(1,9) 
+            print(f"{x} + {y} = ")
+            response = int(input())
+            counter += 1
     elif level == 2:
-        x = random.randint(10,99)
-        y = random.randint(10,99)
-
+        for i in range(1,counter+1):
+            x = random.randint(10,99)
+            y = random.randint(10,99) 
+            print(f"{x} + {y} = ")
+            response = int(input())
+            counter += 1
     elif level == 3:
-        x = random.randint(100,999)
-        y = random.randint(100,999)
-
+        for i in range(1,counter+1):
+            x = random.randint(100,999)
+            y = random.randint(100,999) 
+            print(f"{x} + {y} = ")
+            response = int(input())
+            counter += 1              
+        
+    else:
+        print("Something's up with level generation")
+        raise ValueError
 
 if __name__ == "__main__":
     main()
