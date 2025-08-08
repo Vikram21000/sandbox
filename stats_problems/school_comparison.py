7a68fa9cbca8abfba8ef80041377ff1e754aac7f
Vikram J <148868830+Vikram21000@users.noreply.github.com>
2025-08-08 17:44:04 +0100

    Score normalisation basic functionality

--- stats_problems/school_comparison.py
+++ stats_problems/school_comparison.py
@@ -2,17 +2,12 @@ import pandas as pd
 import statistics as stats
 
 scores = pd.read_csv(r"C:\Users\vikra\Downloads\school_scores.csv")
-def main():
-    percent_score()
-    scores = scores.drop(columns=["Score", "Max Score"])
-    print(scores)
 
-def percent_score():
-    percentage = []
-    for _, j in scores.iterrows():
-        percent = (j["Score"]/j["Max Score"]) * 100
-        percentage.append(round(percent,2))
-    scores["Percentage score"] = percentage
+percentage = []
+for _, j in scores.iterrows():
+    percent = (j["Score"]/j["Max Score"]) * 100
+    percentage.append(round(percent,2))
+scores["Percentage score"] = percentage
 
 def global_mean():
     global_mean = stats.mean(scores["Percentage score"])
@@ -22,13 +17,31 @@ def global_sd():
     global_sd = stats.stdev(scores["Percentage score"])
     return global_sd
 
-def local_zscore():
-    n_zscore = []
-    c_zscore = []
-    for _, j in scores.iterrows():
-        if j["School"] == "Netherhall":
-            print("Netherhall")
-        else:
-            print("Coleridge")
+def zscore():
+    global combined
+    nhl = scores[scores["School"] == "Netherhall"].copy()
+    nhl_mean = nhl["Score"].mean()
+    nhl_stdev = nhl["Score"].std()
+    nhl["Z-score"] = (nhl["Score"] - nhl_mean) / nhl_stdev
 
-local_zscore()
+    col = scores[scores["School"] == "Coleridge"].copy()
+    col_mean = col["Score"].mean()
+    col_stdev = col["Score"].std()
+    col["Z-score"] = (col["Score"] - col_mean) / col_stdev
+    
+    combined = pd.concat([nhl, col])
+    #print(combined)
+    
+zscore()
+
+def norm_score():
+    norm_score = []
+    for _, j in combined.iterrows():
+        normscore = global_mean() + global_sd() * j["Z-score"]
+        norm_score.append(round(normscore, 2))
+    combined["Normalised score"] = norm_score
+    newcombined = combined.drop(["Score", "Max Score", "Z-score"], axis=1)
+    sort_newcombined = newcombined.sort_values(by="Normalised score", ascending=False)
+    print(sort_newcombined)
+    sort_newcombined.to_csv("NormScores.csv")
+norm_score()
